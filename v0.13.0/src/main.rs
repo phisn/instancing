958bca06c34a85eb1e97de51198be1c0733bb6f6
phisn <phisn@outlook.de>
2024-03-16 18:08:31 +0100

    Update

---
+++ v0.13.0/src/main.rs
@@ -0,0 +1,335 @@
+//! A shader that renders a mesh multiple times in one draw call.
+
+use bevy::{
+    asset::AssetMetaCheck, core_pipeline::core_2d::Transparent2d, ecs::{
+        query::QueryItem,
+        system::{lifetimeless::*, SystemParamItem},
+    }, prelude::*, render::{
+        batching::NoAutomaticBatching,
+        extract_component::{ExtractComponent, ExtractComponentPlugin},
+        mesh::{GpuBufferInfo, MeshVertexBufferLayout},
+        render_asset::RenderAssets,
+        render_phase::{
+            AddRenderCommand, DrawFunctions, PhaseItem, RenderCommand, RenderCommandResult,
+            RenderPhase, SetItemPipeline, TrackedRenderPass,
+        },
+        render_resource::*,
+        renderer::RenderDevice,
+        view::{ExtractedView, NoFrustumCulling},
+        Render, RenderApp, RenderSet,
+    }, sprite::{
+        MaterialMesh2dBundle, Mesh2d, Mesh2dHandle, Mesh2dPipeline, Mesh2dPipelineKey,
+        RenderMesh2dInstances, SetMesh2dBindGroup, SetMesh2dViewBindGroup,
+    }, utils::FloatOrd
+};
+use bytemuck::{Pod, Zeroable};
+
+fn main() {
+    App::new()
+        .insert_resource(AssetMetaCheck::Never)
+        .add_plugins((DefaultPlugins, CustomMaterialPlugin))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
+    commands
+        .spawn((
+            Mesh2dHandle(meshes.add(Rectangle::new(1.0, 1.0))),
+            SpatialBundle::INHERITED_IDENTITY,
+            InstancedMaterialHost::default(),
+            // NOTE: Frustum culling is done based on the Aabb of the Mesh and the GlobalTransform.
+            // As the cube is at the origin, if its Aabb moves outside the view frustum, all the
+            // instanced cubes will be culled.
+            // The InstanceMaterialData contains the 'GlobalTransform' information for this custom
+            // instancing, and that is not taken into account with the built-in frustum culling.
+            // We must disable the built-in frustum culling by adding the `NoFrustumCulling` marker
+            // component to avoid incorrect culling.
+            NoFrustumCulling,
+        ))
+        .with_children(|parent| {
+            (1..=10)
+                .flat_map(|x| (1..=10).map(move |y| (x as f32 / 10.0, y as f32 / 10.0)))
+                .for_each(|(x, y)| {
+                    parent.spawn((
+                        InstancedMaterialChild {
+                            color: Color::hsla(x * 360., y, 0.5, 1.0).as_rgba_f32(),
+                            scale: 1.0,
+                        },
+                        TransformBundle::from_transform(Transform {
+                            translation: Vec3::new(x * 10.0 - 5.0, y * 10.0 - 5.0, 0.0),
+                            ..Default::default()
+                        }),
+                    ));
+                });
+        });
+
+    // camera
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 15.0),
+        projection: OrthographicProjection {
+            far: 1000.,
+            near: -1000.,
+            scale: 0.08,
+            ..Default::default()
+        },
+        ..default()
+    });
+}
+
+#[derive(Component, Default, ExtractComponent, Clone)]
+struct InstancedMaterialHost {
+    pub buffer: Vec<InstanceData>,
+}
+
+#[derive(Component, Clone)]
+struct InstancedMaterialChild {
+    pub color: [f32; 4],
+    pub scale: f32,
+}
+
+pub struct CustomMaterialPlugin;
+
+impl Plugin for CustomMaterialPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(ExtractComponentPlugin::<InstancedMaterialHost>::default());
+        app.add_systems(Last, prepare_buffer);
+
+        app.sub_app_mut(RenderApp)
+            .add_render_command::<Transparent2d, DrawCustom>()
+            .init_resource::<SpecializedMeshPipelines<CustomPipeline>>()
+            .add_systems(
+                Render,
+                (
+                    queue_custom.in_set(RenderSet::QueueMeshes),
+                    prepare_instance_buffers.in_set(RenderSet::PrepareResources),
+                ),
+            );
+    }
+
+    fn finish(&self, app: &mut App) {
+        app.sub_app_mut(RenderApp).init_resource::<CustomPipeline>();
+    }
+}
+
+fn prepare_buffer(
+    mut instanced_materials: Query<(&mut InstancedMaterialHost, &Children)>,
+    instanced_material_children: Query<(&InstancedMaterialChild, &Transform)>,
+) {
+    for (mut instanced_material, children) in &mut instanced_materials {
+        let children = children
+            .iter()
+            .map(|entity| instanced_material_children.get(*entity).unwrap());
+
+        instanced_material.buffer.clear();
+
+        for (child, child_transform) in children {
+            instanced_material.buffer.push(InstanceData {
+                position: child_transform.translation,
+                scale: child.scale,
+                color: child.color,
+            });
+        }
+    }
+}
+
+#[derive(Clone, Copy, Pod, Zeroable)]
+#[repr(C)]
+struct InstanceData {
+    position: Vec3,
+    scale: f32,
+    color: [f32; 4],
+}
+
+#[allow(clippy::too_many_arguments)]
+fn queue_custom(
+    transparent_2d_draw_functions: Res<DrawFunctions<Transparent2d>>,
+    custom_pipeline: Res<CustomPipeline>,
+    msaa: Res<Msaa>,
+    mut pipelines: ResMut<SpecializedMeshPipelines<CustomPipeline>>,
+    pipeline_cache: Res<PipelineCache>,
+    meshes: Res<RenderAssets<Mesh>>,
+    render_mesh_instances: Res<RenderMesh2dInstances>,
+    material_meshes: Query<Entity, With<InstancedMaterialHost>>,
+    mut views: Query<(&ExtractedView, &mut RenderPhase<Transparent2d>)>,
+) {
+    let draw_custom = transparent_2d_draw_functions.read().id::<DrawCustom>();
+
+    let msaa_key = Mesh2dPipelineKey::from_msaa_samples(msaa.samples());
+
+    for (view, mut transparent_phase) in &mut views {
+        let view_key = msaa_key | Mesh2dPipelineKey::from_hdr(view.hdr);
+        for entity in &material_meshes {
+            let Some(mesh_instance) = render_mesh_instances.get(&entity) else {
+                continue;
+            };
+            let Some(mesh) = meshes.get(mesh_instance.mesh_asset_id) else {
+                continue;
+            };
+            let key =
+                view_key | Mesh2dPipelineKey::from_primitive_topology(mesh.primitive_topology);
+
+            let pipeline =
+                pipelines.specialize(&pipeline_cache, &custom_pipeline, key, &mesh.layout);
+
+            let pipeline = match pipeline {
+                Ok(id) => id,
+                Err(err) => {
+                    error!("{}", err);
+                    continue;
+                }
+            };
+
+            let mesh_z = mesh_instance.transforms.transform.translation.z;
+
+            transparent_phase.add(Transparent2d {
+                sort_key: FloatOrd(mesh_z),
+                entity: entity,
+                pipeline,
+                draw_function: draw_custom,
+                batch_range: 0..1,
+                dynamic_offset: None,
+            });
+        }
+    }
+}
+
+#[derive(Component)]
+pub struct InstanceBuffer {
+    buffer: Buffer,
+    length: usize,
+}
+
+fn prepare_instance_buffers(
+    mut commands: Commands,
+    query: Query<(Entity, &InstancedMaterialHost)>,
+    render_device: Res<RenderDevice>,
+) {
+    for (entity, instances) in &query {
+        let buffer = render_device.create_buffer_with_data(&BufferInitDescriptor {
+            label: Some("instance data buffer"),
+            contents: bytemuck::cast_slice(instances.buffer.as_slice()),
+            usage: BufferUsages::VERTEX | BufferUsages::COPY_DST,
+        });
+
+        commands.entity(entity).insert(InstanceBuffer {
+            buffer,
+            length: instances.buffer.len(),
+        });
+    }
+}
+
+#[derive(Resource)]
+pub struct CustomPipeline {
+    shader: Handle<Shader>,
+    mesh_pipeline: Mesh2dPipeline,
+}
+
+impl FromWorld for CustomPipeline {
+    fn from_world(world: &mut World) -> Self {
+        let asset_server = world.resource::<AssetServer>();
+        let shader = asset_server.load("shaders/instancing.wgsl");
+
+        let mesh_pipeline = world.resource::<Mesh2dPipeline>();
+
+        CustomPipeline {
+            shader,
+            mesh_pipeline: mesh_pipeline.clone(),
+        }
+    }
+}
+
+impl SpecializedMeshPipeline for CustomPipeline {
+    type Key = Mesh2dPipelineKey;
+
+    fn specialize(
+        &self,
+        key: Self::Key,
+        layout: &MeshVertexBufferLayout,
+    ) -> Result<RenderPipelineDescriptor, SpecializedMeshPipelineError> {
+        let mut descriptor = self.mesh_pipeline.specialize(key, layout)?;
+
+        // meshes typically live in bind group 2. because we are using bindgroup 1
+        // we need to add MESH_BINDGROUP_1 shader def so that the bindings are correctly
+        // linked in the shader
+        descriptor
+            .vertex
+            .shader_defs
+            .push("MESH_BINDGROUP_1".into());
+
+        descriptor.vertex.shader = self.shader.clone();
+        descriptor.vertex.buffers.push(VertexBufferLayout {
+            array_stride: std::mem::size_of::<InstanceData>() as u64,
+            step_mode: VertexStepMode::Instance,
+            attributes: vec![
+                VertexAttribute {
+                    format: VertexFormat::Float32x4,
+                    offset: 0,
+                    shader_location: 3, // shader locations 0-2 are taken up by Position, Normal and UV attributes
+                },
+                VertexAttribute {
+                    format: VertexFormat::Float32x4,
+                    offset: VertexFormat::Float32x4.size(),
+                    shader_location: 4,
+                },
+            ],
+        });
+        descriptor.fragment.as_mut().unwrap().shader = self.shader.clone();
+        Ok(descriptor)
+    }
+}
+
+type DrawCustom = (
+    SetItemPipeline,
+    SetMesh2dViewBindGroup<0>,
+    SetMesh2dBindGroup<1>,
+    DrawMeshInstanced,
+);
+
+pub struct DrawMeshInstanced;
+
+impl<P: PhaseItem> RenderCommand<P> for DrawMeshInstanced {
+    type Param = (SRes<RenderAssets<Mesh>>, SRes<RenderMesh2dInstances>);
+    type ViewQuery = ();
+    type ItemQuery = Read<InstanceBuffer>;
+
+    #[inline]
+    fn render<'w>(
+        item: &P,
+        _view: (),
+        instance_buffer: Option<&'w InstanceBuffer>,
+        (meshes, render_mesh_instances): SystemParamItem<'w, '_, Self::Param>,
+        pass: &mut TrackedRenderPass<'w>,
+    ) -> RenderCommandResult {
+        let Some(mesh_instance) = render_mesh_instances.get(&item.entity()) else {
+            return RenderCommandResult::Failure;
+        };
+        let gpu_mesh = match meshes.into_inner().get(mesh_instance.mesh_asset_id) {
+            Some(gpu_mesh) => gpu_mesh,
+            None => return RenderCommandResult::Failure,
+        };
+
+        let instance_buffer = match instance_buffer {
+            Some(instance_buffer) => instance_buffer,
+            None => return RenderCommandResult::Failure,
+        };
+
+        pass.set_vertex_buffer(0, gpu_mesh.vertex_buffer.slice(..));
+        pass.set_vertex_buffer(1, instance_buffer.buffer.slice(..));
+
+        match &gpu_mesh.buffer_info {
+            GpuBufferInfo::Indexed {
+                buffer,
+                index_format,
+                count,
+            } => {
+                pass.set_index_buffer(buffer.slice(..), 0, *index_format);
+                pass.draw_indexed(0..*count, 0, 0..instance_buffer.length as u32);
+            }
+            GpuBufferInfo::NonIndexed => {
+                pass.draw(0..gpu_mesh.vertex_count, 0..instance_buffer.length as u32);
+            }
+        }
+        RenderCommandResult::Success
+    }
+}
